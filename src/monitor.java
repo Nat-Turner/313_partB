ba337fdfcacc2095265814fc906d2eb4bd74a87d
Yukeung Michael Chan <emb14134@uni.strath.ac.uk>
2016-10-13 21:20:33 +0100

    gets all Active Groups and also all Active Threads (i think)

--- src/monitor.java
+++ src/monitor.java
@@ -8,11 +8,40 @@ public class monitor {
     public static void main(String[] args) {
         monitor m = new monitor();
         ThreadGroup root = m.getRoot();
-        m.printGroup(root);
+        ThreadGroup[] groups = m.getAllGroups();
+
+        System.out.println("All Active Groups");
+        System.out.println(m.getRoot().getName());
+        m.printGroup(groups);
+
+        System.out.println("");
+        System.out.println("ALL Active Threads :");
+        m.getAllThreads(groups);
 
 
 
     }
+
+
+    public void getAllThreads(ThreadGroup[] g){
+        int NoGroups=g.length;
+        for(int i =0; i<NoGroups; i++){
+           Thread[] t = threadsInGroup(g[i]);
+            int threadsInGroup=t.length;
+            for(int a=0; a<threadsInGroup; a++){
+                System.out.println(t[a].getName());
+            }
+        }
+
+    }
+    public Thread[] threadsInGroup(ThreadGroup g){
+        int threadcount = g.activeCount();
+        Thread[] threads = new Thread[threadcount];
+        g.enumerate(threads);
+        return(threads);
+    }
+
+
     public ThreadGroup getRoot(){
         if( Thread.currentThread().getThreadGroup().getParent() == null){
             return Thread.currentThread().getThreadGroup().getParent() ;// if the current thread doesn't have a parent the current thread group is the root
@@ -24,10 +53,21 @@ public class monitor {
         }
         return currentGroup;
     }
-    
 
-    public void printGroup(ThreadGroup g){
-        System.out.println(g.getName());
+    public ThreadGroup[] getAllGroups(){
+        ThreadGroup root = getRoot();
+        int numberOfGroups = root.activeGroupCount();
+        ThreadGroup[] groups = new ThreadGroup[numberOfGroups];
+
+        root.enumerate(groups,true);
+
+        return groups; // doesnt include the root
+    }
+    public void printGroup(ThreadGroup[] g){
+        int count  = g.length;
+        for(int i=0; i<count;i++) {
+            System.out.println(g[i].getName());
+        }
     }
     /*So far only Displays ID, Name and if thread is Daemon or not*/
     public void DisplayThreads(Thread list[]){
