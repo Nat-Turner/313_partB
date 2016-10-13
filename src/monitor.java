a93020d85463fbd9e15623086dd23347a9350343
Yukeung Michael Chan <emb14134@uni.strath.ac.uk>
2016-10-13 20:37:57 +0100

    finding the root group

--- src/monitor.java
+++ src/monitor.java
@@ -3,13 +3,31 @@ import java.lang.Thread;
 import java.util.ArrayList;
 
 public class monitor {
+
+
     public static void main(String[] args) {
-      monitor m = new monitor();
-        Thread processes[] = m.activeThreads();
-        m.DisplayThreads(processes);
+        monitor m = new monitor();
+        ThreadGroup root = m.getRoot();
+        m.printGroup(root);
 
 
 
+    }
+    public ThreadGroup getRoot(){
+        if( Thread.currentThread().getThreadGroup().getParent() == null){
+            return Thread.currentThread().getThreadGroup().getParent() ;// if the current thread doesn't have a parent the current thread group is the root
+        }
+        ThreadGroup currentGroup = Thread.currentThread().getThreadGroup(); // this is getting the current group of a thread
+        ThreadGroup ParentGroup;
+        while ((ParentGroup = currentGroup.getParent())!=null){
+            currentGroup=ParentGroup;
+        }
+        return currentGroup;
+    }
+    
+
+    public void printGroup(ThreadGroup g){
+        System.out.println(g.getName());
     }
     /*So far only Displays ID, Name and if thread is Daemon or not*/
     public void DisplayThreads(Thread list[]){
@@ -28,8 +46,12 @@ public class monitor {
 
         }
     }
-
-
+    public void ThreadGroup(Thread list[]){
+        int Count = list.length;
+        for (int i = 0; i < Count; i++) {
+            System.out.println("The Thread "+list[i].getName()+" belongs to group :" + list[i].getThreadGroup());
+        }
+    }
     public Thread[] activeThreads(){
         int Count = Thread.activeCount();
         System.out.println("number of active Threads " + Count);
